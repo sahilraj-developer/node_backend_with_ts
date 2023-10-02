2c3a797d27ac97a5bccd76f73c87ab6d32f58425
sahil <sahil27raj@gmail.com>
2023-10-02 22:58:52 +0530

    commit the backend project with the typescript

---
+++ app.ts
@@ -0,0 +1,8 @@
+let message: string = "hello world"
+console.log(message);
+
+function compute(a:number ,b:number){
+    return a = b;
+}
+
+console.log(compute(2,2))
---
+++ readme.txt
@@ -0,0 +1,7 @@
+npm init -y 
+npm i typescript -D
+tsc --init
+less tsconfig.json
+
+tsc    -> to compile your code 
+node app.js   -> to run the code 
